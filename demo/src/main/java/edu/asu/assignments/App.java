0d5a69d4e7540d88905ab44bb03c16dd680a2e1e
Coding Rooms <git@codingrooms.io>
2023-07-08 14:55:38 +0000

    first commit

---
+++ demo/src/main/java/edu/asu/assignments/App.java
@@ -0,0 +1,17 @@
+package edu.asu.assignments;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public static int sum(int x, int y) {
+        return x + y;
+    }
+}
---
+++ demo/src/test/java/edu/asu/assignments/AppTest.java
@@ -0,0 +1,21 @@
+package edu.asu.assignments;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertEquals(App.sum(1,1), 2);
+    }
+}
